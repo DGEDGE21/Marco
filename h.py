ec49a5bc54be26a17e44b97c55a3d0b3ca7c77e4
DGEDGE21 <46941888+DGEDGE21@users.noreply.github.com>
2019-02-15 22:45:26 +0200

    fase2\ Semana2

---
+++ h.py
@@ -0,0 +1,16 @@
+class Individuo:
+    def __init__(self,nomep ,apelido ,altura ,naturalidade,estadocivil):
+        self.nomep=nomep
+        self.apelido=apelido
+        self.altura=altura
+        self.naturalidade=naturalidade
+        self.estadocivil=estadocivil
+
+class Filiacao:
+    def __init__(self,pai,mae):
+        self.pai=pai
+        self.mae=mae
+class Morada :
+    def __init__(self,pais,localidade):
+        self.pais=pais
+        self.localidade=localidade
